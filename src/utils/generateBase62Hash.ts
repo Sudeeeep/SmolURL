d195ca5824a7fcf6f014963def6d1e858a98395e
Sudeep <sudeepn15@gmail.com>
2023-11-06 23:51:14 +0530

    add generateBase62 helper function

---
+++ src/utils/generateBase62Hash.ts
@@ -0,0 +1,16 @@
+export function generateBase62Hash(counter: number) {
+  const base62 =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let hash = "";
+  let temp = counter;
+  while (temp > 0) {
+    let digit = temp % 62;
+    hash += base62[digit];
+    temp = Math.floor(temp / 10);
+  }
+
+  while (hash.length < 7) {
+    hash += base62[Math.floor(Math.random() * 63)];
+  }
+  return hash;
+}
