51d746dc25706ddce5874bce1741bbef1e50fd60
Sudeep <sudeepn15@gmail.com>
2023-11-14 17:59:17 +0530

    add CreateAccount componenet

--- client/src/components/Body.tsx
+++ client/src/components/Body.tsx
@@ -1,9 +1,11 @@
+import { CreateAccount } from "./CreateAccount";
 import { SmolUrl } from "./SmolUrl";
 
 export const Body = () => {
   return (
     <div>
       <SmolUrl />
+      <CreateAccount />
     </div>
   );
 };
---
+++ client/src/components/CreateAccount.tsx
@@ -0,0 +1,16 @@
+export const CreateAccount = () => {
+  return (
+    <div className="max-w-full min-[770px]:max-w-[80%] mx-auto mt-24 mb-10 text-center">
+      <h1 className="text-3xl font-bold mb-2">Need more features?</h1>
+
+      <p className="mb-8">
+        Sign Up to save your SmolURLs and get statistics like the number of
+        users that have clicked on your link.
+      </p>
+
+      <button className="py-4 px-8 mx-auto bg-[#E3A64A] rounded-lg">
+        Create Account
+      </button>
+    </div>
+  );
+};
