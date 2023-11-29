af221cf69e3d6ed801e967b2387ed5181365cc15
Sudeep <sudeepn15@gmail.com>
2023-11-29 18:27:35 +0530

    fix checkProtocol to add protocols to only strings that are url

--- server/src/utils/checkProtocol.ts
+++ server/src/utils/checkProtocol.ts
@@ -2,7 +2,12 @@
 export function checkProtocol(url: string) {
   const protocolRegex = /^(ftp|http|https):\/\//;
 
-  if (!protocolRegex.test(url)) {
+  if (
+    !protocolRegex.test(url) &&
+    url.match(
+      /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi
+    )
+  ) {
     console.log("no protocol");
     return "http://" + url;
   }
