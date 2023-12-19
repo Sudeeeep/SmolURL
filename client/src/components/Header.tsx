41449fc7ae1e726b2542f265f807f11c48552198
Sudeep <sudeepn15@gmail.com>
2023-12-19 17:53:48 +0530

    redirect user to homepage when logo is clicked

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,10 +4,10 @@ import smolLogo from "../assets/smol.png";
 export const Header = () => {
   return (
     <div className="flex justify-between py-2">
-      <div className="flex items-center gap-2">
+      <Link to="/" className="flex items-center gap-2">
         <img src={smolLogo} alt="SmolURL" className="w-10" />
         <h1 className="text-3xl sm:text-4xl">SmolURL</h1>
-      </div>
+      </Link>
       <Link
         to={"/login"}
         className="bg-[#E3A64A] px-3 py-1 sm:px-6 sm:py-2 rounded-md"
